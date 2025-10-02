25e55ca7f790f7c4d82b906ff454c70d2c20a809
Sophy <sssoz@users.noreply.github.com>
2025-10-02 11:36:50 +0100

    Add more specific syp redirects pass through redirects so that Netlify can pick it up
    See oaworks/discussion#3557

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("_redirects");
   eleventyConfig.addPassthroughCopy({ "src/embed.js": "embed.js" });
   eleventyConfig.addPassthroughCopy({ "src/icon_OAB.png": "icon_OAB.png" });
+  eleventyConfig.addPassthroughCopy({ "src/book.jpg": "book.jpg" });
   eleventyConfig.addPassthroughCopy({ "src/book.jpg": "book.jpg" });
   
   return {
